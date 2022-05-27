d92170972da7e6f27c3fabf8c501a4bd40b5a9e6
RuiSilva97 <80705932+RuiSilva97@users.noreply.github.com>
2022-05-27 17:48:53 +0100

    StartDashboard

--- Kubernetes-GUI/Forms/DashboardForm.Designer.cs
+++ Kubernetes-GUI/Forms/DashboardForm.Designer.cs
@@ -29,26 +29,145 @@ namespace Kubernetes_GUI.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartPods = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.nameColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.nameDepColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.imagesPodColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nodePodsColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPods)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
+            // 
+            // chartPods
+            // 
+            chartArea4.Name = "Pods";
+            this.chartPods.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chartPods.Legends.Add(legend4);
+            this.chartPods.Location = new System.Drawing.Point(827, 270);
+            this.chartPods.Name = "chartPods";
+            series4.ChartArea = "Pods";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series4.Legend = "Legend1";
+            series4.Name = "Pods";
+            this.chartPods.Series.Add(series4);
+            this.chartPods.Size = new System.Drawing.Size(300, 300);
+            this.chartPods.TabIndex = 0;
+            this.chartPods.Text = "chartPods";
+            this.chartPods.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameColumn,
+            this.imagesPodColumn1,
+            this.nodePodsColumn1,
+            this.statusColumn});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1139, 99);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameColumn1});
+            this.dataGridView2.Location = new System.Drawing.Point(0, 158);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(240, 104);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // nameColumn1
+            // 
+            this.nameColumn1.HeaderText = "Name";
+            this.nameColumn1.Name = "nameColumn1";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDepColumn1});
+            this.dataGridView3.Location = new System.Drawing.Point(0, 333);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(240, 90);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // nameDepColumn1
+            // 
+            this.nameDepColumn1.HeaderText = "Name";
+            this.nameDepColumn1.Name = "nameDepColumn1";
+            // 
+            // nameColumn
+            // 
+            this.nameColumn.HeaderText = "Name";
+            this.nameColumn.Name = "nameColumn";
+            // 
+            // imagesPodColumn1
+            // 
+            this.imagesPodColumn1.HeaderText = "Images";
+            this.imagesPodColumn1.Name = "imagesPodColumn1";
+            // 
+            // nodePodsColumn1
+            // 
+            this.nodePodsColumn1.HeaderText = "Node";
+            this.nodePodsColumn1.Name = "nodePodsColumn1";
+            // 
+            // statusColumn
+            // 
+            this.statusColumn.HeaderText = "Status";
+            this.statusColumn.Name = "statusColumn";
             // 
             // DashboardForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1519, 716);
+            this.ClientSize = new System.Drawing.Size(1139, 582);
             this.ControlBox = false;
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chartPods);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "DashboardForm";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "DashboardForm";
+            ((System.ComponentModel.ISupportInitialize)(this.chartPods)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartPods;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameColumn1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDepColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn imagesPodColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nodePodsColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn statusColumn;
     }
 }
--- Kubernetes-GUI/Forms/DashboardForm.cs
+++ Kubernetes-GUI/Forms/DashboardForm.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Kubernetes_GUI.Models;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,127 @@ namespace Kubernetes_GUI.Forms
         public DashboardForm()
         {
             InitializeComponent();
+
+            getPods();
+            getDep();
+            getServ();
+        }
+
+        private void getPods()
+        {
+            string url = GlobalSessionDetails.Protocol + "://" + GlobalSessionDetails.Domain + ":" + GlobalSessionDetails.Port + "/api/v1/pods";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var client = GlobalSessionDetails._clientFactory.CreateClient();
+
+            var response = client.SendAsync(request).Result;
+            var json = response.Content.ReadAsStringAsync().Result;
+
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show(response.ReasonPhrase, "Could not get the Pods", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            JObject responseJsonObject = JObject.Parse(json);
+            JArray pods = (JArray)responseJsonObject["items"];
+
+            for (int i = 0; i < pods.Count; i++)
+            {
+                var currentPod = pods[i];
+
+                var name = currentPod["metadata"]["name"];
+
+                HashSet<string> imagesList = new HashSet<string>();
+                foreach (var item in currentPod["spec"]["containers"])
+                {
+                    imagesList.Add(item["image"].ToString());
+                }
+
+                string images = null;
+                if (imagesList.Count > 0)
+                {
+                    images = string.Join("\n", imagesList.ToArray());
+                }
+
+                var node = currentPod["spec"]["nodeName"];
+
+                var status = currentPod["status"]["phase"];
+
+                dataGridView1.Rows.Add(
+                    name is null ? "" : name.ToString(),
+                    images is null ? "" : images.ToString(),
+                    node is null ? "" : node.ToString(),
+                    status is null ? "" : status.ToString());                    
+            }
+
+        }
+
+        private void getServ()
+        {
+            string url = GlobalSessionDetails.Protocol + "://" + GlobalSessionDetails.Domain + ":" + GlobalSessionDetails.Port + "/api/v1/services";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var client = GlobalSessionDetails._clientFactory.CreateClient();
+
+            var response = client.SendAsync(request).Result;
+            var json = response.Content.ReadAsStringAsync().Result;
+
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show(response.ReasonPhrase, "Could not get the Pods", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            JObject responseJsonObject = JObject.Parse(json);
+            JArray services = (JArray)responseJsonObject["items"];
+
+            for (int i = 0; i < services.Count; i++)
+            {
+                var currentService = services[i];
+
+                var name = currentService["metadata"]["name"];
+
+                dataGridView2.Rows.Add(name is null ? "" : name.ToString());
+            }
+        }
+
+        private void getDep()
+        {
+            string url = GlobalSessionDetails.Protocol + "://" + GlobalSessionDetails.Domain + ":" + GlobalSessionDetails.Port + "/apis/apps/v1/deployments";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var client = GlobalSessionDetails._clientFactory.CreateClient();
+
+            var response = client.SendAsync(request).Result;
+            var json = response.Content.ReadAsStringAsync().Result;
+
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show(response.ReasonPhrase, "Could not get the Pods", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            JObject responseJsonObject = JObject.Parse(json);
+            JArray pods = (JArray)responseJsonObject["items"];
+
+            for (int i = 0; i < pods.Count; i++)
+            {
+                var currentPod = pods[i];
+
+                var name = currentPod["metadata"]["name"];
+
+                var status = currentPod["status"]["phase"];
+
+                dataGridView3.Rows.Add(
+                    name is null ? "" : name.ToString());
+            }
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
